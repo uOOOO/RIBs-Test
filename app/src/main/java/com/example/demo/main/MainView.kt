357dd22ed2a74d8d3e27f8020671e61b87b797dc
SeungHun Choe <13363349+uOOOO@users.noreply.github.com>
2020-02-12 17:12:35 +0900

    Modify test code

--- app/src/main/java/com/example/demo/main/MainView.kt
+++ app/src/main/java/com/example/demo/main/MainView.kt
@@ -24,51 +24,50 @@ class MainView @JvmOverloads constructor(
 
   override fun onFinishInflate() {
     super.onFinishInflate()
-//    button.setOnClickListener {
-//      button.text = "CLICKED!!!"
-//      (context as Activity).startActivityForResult(Intent(context, AnotherActivity::class.java), 0)
-//    }
   }
 
   override fun showSubView(): Observable<Unit> {
     return button.clicks()
-  }
-
-  override fun onSaveInstanceState(): Parcelable? {
-    val state = super.onSaveInstanceState()
-    val newState = TestState(state)
-    newState.value = this.button.text.toString()
-    return newState
-  }
-
-  override fun onRestoreInstanceState(state: Parcelable?) {
-    val testState = state as TestState
-    super.onRestoreInstanceState(testState.superState)
-    button.text = testState.value
-  }
-
-  private class TestState : BaseSavedState {
-    var value: String? = null
-
-    constructor(superState: Parcelable?) : super(superState)
-
-    constructor(`in`: Parcel) : super(`in`) {
-      value = `in`.readString()
-    }
-
-    override fun writeToParcel(out: Parcel, flags: Int) {
-      super.writeToParcel(out, flags)
-      out.writeString(value)
-    }
-
-    companion object CREATOR : Parcelable.Creator<TestState> {
-      override fun createFromParcel(parcel: Parcel): TestState {
-        return TestState(parcel)
+      .doOnNext {
+        button.text = "MAIN CLICKED!!!"
       }
-
-      override fun newArray(size: Int): Array<TestState?> {
-        return arrayOfNulls(size)
-      }
-    }
   }
+
+//  override fun onSaveInstanceState(): Parcelable? {
+//    val state = super.onSaveInstanceState()
+//    val newState = TestState(state)
+//    newState.value = this.button.text.toString()
+//    return newState
+//  }
+//
+//  override fun onRestoreInstanceState(state: Parcelable?) {
+//    val testState = state as TestState
+//    super.onRestoreInstanceState(testState.superState)
+//    button.text = testState.value
+//  }
+//
+//  private class TestState : BaseSavedState {
+//    var value: String? = null
+//
+//    constructor(superState: Parcelable?) : super(superState)
+//
+//    constructor(`in`: Parcel) : super(`in`) {
+//      value = `in`.readString()
+//    }
+//
+//    override fun writeToParcel(out: Parcel, flags: Int) {
+//      super.writeToParcel(out, flags)
+//      out.writeString(value)
+//    }
+//
+//    companion object CREATOR : Parcelable.Creator<TestState> {
+//      override fun createFromParcel(parcel: Parcel): TestState {
+//        return TestState(parcel)
+//      }
+//
+//      override fun newArray(size: Int): Array<TestState?> {
+//        return arrayOfNulls(size)
+//      }
+//    }
+//  }
 }
