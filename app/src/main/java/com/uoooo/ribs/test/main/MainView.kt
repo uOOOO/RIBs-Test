caffba38a6c4e992c4d4cf5a5e0bb1a2f485d795
SeungHun Choe <13363349+uOOOO@users.noreply.github.com>
2020-02-12 16:59:51 +0900

    Add list view

--- app/src/main/java/com/uoooo/ribs/test/main/MainView.kt
+++ app/src/main/java/com/uoooo/ribs/test/main/MainView.kt
@@ -2,9 +2,16 @@ package com.uoooo.ribs.test.main
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.rxbinding3.view.clicks
+import com.uoooo.ribs.test.R
 import io.reactivex.Observable
+import kotlinx.android.synthetic.main.list_item.view.*
 import kotlinx.android.synthetic.main.main_rib.view.*
 
 
@@ -12,20 +19,46 @@ import kotlinx.android.synthetic.main.main_rib.view.*
  * Top level view for {@link MainBuilder.MainScope}.
  */
 class MainView @JvmOverloads constructor(
-  context: Context,
-  attrs: AttributeSet? = null,
-  defStyle: Int = 0
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
 ) : FrameLayout(context, attrs, defStyle), MainInteractor.MainPresenter {
 
   override fun onFinishInflate() {
     super.onFinishInflate()
+    list.layoutManager = LinearLayoutManager(context)
+    list.adapter = TestAdapter()
+  }
+
+  class TestAdapter : RecyclerView.Adapter<TestAdapter.TestViewHolder>() {
+    private val data: List<String> = mutableListOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+        "K", "L", "M", "N", "O", "P")
+
+    class TestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+      fun bind(data: String) {
+        itemView.text.text = data
+      }
+    }
+
+    override fun getItemCount(): Int {
+      return data.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder {
+      return TestViewHolder(
+          LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: TestViewHolder, position: Int) {
+      holder.bind(data[position])
+    }
   }
 
   override fun showSubView(): Observable<Unit> {
     return button.clicks()
-      .doOnNext {
-        button.text = "MAIN CLICKED!!!"
-      }
+        .doOnNext {
+          button.text = "MAIN CLICKED!!!"
+        }
   }
 
 //  override fun onSaveInstanceState(): Parcelable? {
